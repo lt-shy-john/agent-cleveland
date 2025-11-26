7d41bfbfde8f7b12de1ee0fbb9bf654995fca6b5
John Yeung <john@shyjohn.net>
2025-11-26 21:14:12 +1100

    Added the agents

--- lang-agent/agent.py
+++ lang-agent/agent.py
@@ -1,5 +1,4 @@
-from google.adk.agents import Agent
-from google.adk.tools import FunctionTool
+from google.adk.agents import Agent, SequentialAgent, ParallelAgent, LoopAgent
 from google.adk.runners import InMemoryRunner
 from google.adk.plugins.logging_plugin import LoggingPlugin
 from google.genai import types
@@ -7,10 +6,9 @@ import asyncio
 
 from .config import config
 from plugin.CountInvocationPlugin import CountInvocationPlugin
-from .tools import find_lang_agent
 
-root_agent = Agent(
-    name="language_teaching_agent",
+init_agent = Agent(
+    name="language_teaching_init_agent",
     model=config.worker_model,
     description="The lang-agent which teaches user about a foreign language.",
     instruction=f"""
@@ -18,15 +16,62 @@ root_agent = Agent(
 
     Your workflow is as follows:
     1.  **Plan:** The user will ask help for one language. To do this, use the `find_lang_agent` tool.
-    2.  **Teach** You provide them one example of using the language. 
+    2.  **Decide** You then ask what the user want to do today. They will prompt you based on the following options: 
+        *  **Teach** You provide them one example of using the language. 
+        *  **Conversation** You and the user talks with the language casually. 
+        *  **Quiz** You ask the user a word from the language.  
 
     If you are asked what is your name respond with Agent Cleveland.
     """,
-    sub_agents=[
-        find_lang_agent,
-    ],
     tools=[],
-    output_key="blog_outline",
+    output_key="conversation",
+)
+
+find_lang_agent = Agent(
+    name="find_lang_agent",
+    model=config.worker_model,
+    instruction="""Find out what language is the user after. """,
+    output_key="lang",
+)
+
+teacher_agent = Agent(
+    name="teacher_agent",
+    model=config.worker_model,
+    instruction="""Pick any word from the language {lang}, tell the user the meaning of this word. """,
+    output_key="vocab_meaning",
+)
+
+conversation_agent = Agent(
+    name="conversation_agent",
+    model=config.worker_model,
+    instruction="""You are a casual conversation agent, when the user gives prompts you answer them using the language {lang}. """,
+    output_key="sentence",
+)
+
+quiz_agent = Agent(
+    name="quiz_agent",
+    model=config.worker_model,
+    instruction="""Ask the user one of the meaning of the words from {lang}. If the user gives the response similar to the word's meaning, then give one point to the user. """,
+    output_key="point",
+)
+
+parallel_lang_agent = ParallelAgent(
+    name="ParallelLangTeam",
+    sub_agents=[teacher_agent, conversation_agent, quiz_agent],
+)
+
+conversation_refinement_loop = LoopAgent(
+    name="LangConversationRefinementLoop",
+    sub_agents=[parallel_lang_agent],
+    max_iterations=10,  # Prevents infinite loops
+)
+
+root_agent = SequentialAgent(
+    name="language_teaching_agent",
+    description="The lang-agent which teaches user about a foreign language.",
+    sub_agents=[
+        init_agent, conversation_refinement_loop
+    ],
 )
 
 async def main():
